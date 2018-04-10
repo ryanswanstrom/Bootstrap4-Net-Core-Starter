96ac594a3ba7482dc0927bda4c7f314fedba3a70
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2018-04-10 08:43:34 -0500

    Add project files.

---
+++ Bootstrap4-Net-Core-Starter/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap4_Net_Core_Starter.Models.AccountViewModels
+{
+    public class LoginWithRecoveryCodeViewModel
+    {
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Recovery Code")]
+            public string RecoveryCode { get; set; }
+    }
+}
